794446d05cc4010bd601f2cee2a3e9d0568bf7c8
yakebashir <98105123+yakebashir@users.noreply.github.com>
2023-07-10 14:17:35 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -41,19 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //Added home property
-      home: const HomePage(),
     );
-  }
-}
-
-//Home Page Widget
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text('hee');
   }
 }
 
