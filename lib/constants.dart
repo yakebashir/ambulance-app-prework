f109865b4e53495f86cdbad93065db16bed04921
Mukasa-Joshua1 <107024716+Mukasa-Joshua1@users.noreply.github.com>
2023-07-17 12:41:09 +0300

    Added constants.dart file into lib
    I've created our Colours. We can change them later but for now they will help us create something uniform as we design the pages. Feel free to edit the Colours.

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Our Colors
+const Color kViolet = Color.fromRGBO(128, 0, 255, 1);
+const Color kLightViolet = Color.fromRGBO(209, 164, 255, 1);
+const Color kWhite = Colors.white;
+const Color kBlack = Colors.black87;
+const Color kGrey = Colors.grey;
+final Color kLightGrey = Colors.grey.shade400;
